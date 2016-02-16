89a7e8641fc6bbe8397980771a3cc74a7f6a2c64
Oleksandr.Zinevych <aoleksandr.zinevych@sethq.com>
2016-02-16 17:43:32 +0200

    renamed middleware

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,31 +1,31 @@
 var express = require('express'),
-  router = express.Router(),
-  User = require('../models/user'),
-    jwtCheck = require('../middlewares/jwtCheck');
+    router = express.Router(),
+    User = require('../models/user'),
+    auth = require('../middlewares/auth');
 
 module.exports = function (app) {
   app.use('/users', router);
 };
 
-router.get('/setup', function(req, res) {
+//router.get('/setup', function(req, res) {
+//
+//  // create a sample user
+//  var nick = new User({
+//    name: 'Nick Cerminara',
+//    password: 'password',
+//    admin: true
+//  });
+//
+//  // save the sample user
+//  nick.save(function(err) {
+//    if (err) throw err;
+//
+//    console.log('User saved successfully');
+//    res.json({ success: true });
+//  });
+//});
 
-  // create a sample user
-  var nick = new User({
-    name: 'Nick Cerminara',
-    password: 'password',
-    admin: true
-  });
-
-  // save the sample user
-  nick.save(function(err) {
-    if (err) throw err;
-
-    console.log('User saved successfully');
-    res.json({ success: true });
-  });
-});
-
-router.get('/all', jwtCheck, function (req, res, next) {
+router.get('/all', auth, function (req, res, next) {
   User.find({}, function(err, users) {
     res.json(users);
   });
