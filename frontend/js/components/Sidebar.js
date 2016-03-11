28f7d22a474b4339a4e1d31cf700ac137f62f11a
Oleksandr.Zinevych <aoleksandr.zinevych@sethq.com>
2016-03-11 18:13:33 +0200

    sidebar initial v

--- frontend/js/components/Sidebar.js
+++ frontend/js/components/Sidebar.js
@@ -5,7 +5,20 @@ export default class Sidebar extends React.Component {
   render() {
     return (
         <div className="sidebar">
+          <div className="sidebar-item">
+            <span>Dashboard</span>
+            <hr/>
+          </div>
 
+          <div className="sidebar-item">
+            <span>Employees</span>
+            <hr/>
+          </div>
+
+          <div className="sidebar-item">
+            <span>Payments</span>
+            <hr/>
+          </div>
         </div>);
   }
 };
